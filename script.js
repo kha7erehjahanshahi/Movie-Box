dbeee57a58ffcb3c56a582b47efd66bfa28319af
kha7erehjahanshahi <111133121+kha7erehjahanshahi@users.noreply.github.com>
2022-12-26 17:14:58 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const targetDiv = document.getElementById("side-nav");
+const btn = document.getElementById("toggle-btn");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";  }
+};
+
+const btnClose = document.getElementById("close-toggle-btn");
+btnClose.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
